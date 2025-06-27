4140160ff85edc250cde0ce03c1cd2b6877ab508
Kenny Kim <caspi46@naver.com>
2025-06-27 16:07:33 -0700

    comments about cargo run commands

--- rget/src/main.rs
+++ rget/src/main.rs
@@ -17,3 +17,32 @@ fn main() {
     let url = matches.value_of("URL").unwrap(); 
     println!("{}", url); 
 }
+
+// cargo run 
+//     Finished dev [unoptimized + debuginfo] target(s) in 0.0 secs
+//      Running `target/debug/rget`
+// error: The following required arguments were not provided:
+//     <URL>
+
+// USAGE:
+//     rget <URL>
+
+// For more information try --help
+
+
+// cargo run -- -h 
+//     Finished dev [unoptimized + debuginfo] target(s) in 0.0 secs
+//      Running `target/debug/rget -h`
+// Rget 0.1.0
+// Matt Gathu <mattgathu@gmail.com>
+// wget clone written in Rust
+
+// USAGE:
+//     rget <URL>
+
+// FLAGS:
+//     -h, --help       Prints help information
+//     -V, --version    Prints version information
+
+// ARGS:
+//     <URL>    url to download
